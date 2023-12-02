dd995d1ee8a14db6291cb82c49c5ad909afcca81
Kashyap Panda <5233012+kashyappanda@users.noreply.github.com>
2023-12-02 17:49:32 -0500

    Reformat lines

--- construct_graph.cpp
+++ construct_graph.cpp
@@ -49,8 +49,7 @@ double calculateDistance(double lat1, double lng1, double lat2, double lng2) {
     lat1 = toRadians(lat1);
     lat2 = toRadians(lat2);
 
-    double a = sin(dLat/2) * sin(dLat/2) +
-               sin(dLng/2) * sin(dLng/2) * cos(lat1) * cos(lat2);
+    double a = sin(dLat/2) * sin(dLat/2) + sin(dLng/2) * sin(dLng/2) * cos(lat1) * cos(lat2);
     double c = 2 * atan2(sqrt(a), sqrt(1-a));
 
     return EARTH_RADIUS_KM * c;
